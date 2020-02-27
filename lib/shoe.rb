f219efeadd752756c809c4853accc4507bb58114
Laila Campbell <lailacampbell96@gmail.com>
2020-02-27 18:51:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,1 @@
-# Make your shoe class here!
-class Shoe
-  attr_accessor :size, :color, :material, :condition
-  attr_reader :brand
- 
-  def initialize(brand)
-    @brand = brand
-  end
- 
-  def initialize(brand)
-    puts "Your shoe is as good as new!"
-      @condition = "new"
-  end
- 
-end
+  @condition = "new"
