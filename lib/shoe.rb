de1a7cdad5d405ee64d0afb5440729579dccebef
Laila Campbell <lailacampbell96@gmail.com>
2020-02-27 18:48:37 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,15 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :size, :color, :material, :condition
+  attr_reader :brand
  
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
  
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+  def initialize(brand)
+    puts "Your shoe is as good as new!"
+      @condition = "new"
   end
  
 end
